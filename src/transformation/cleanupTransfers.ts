979e9882b64b7a276aed00947d5423712fd240e2
Ell Bradshaw <ell@c9a.co>
2024-01-19 01:29:34 -0800

    Fix filters to deal with budget items

--- src/transformation/cleanupTransfers.ts
+++ src/transformation/cleanupTransfers.ts
@@ -67,12 +67,12 @@ export function cleanupMeta(config: IConfiguration, entries: IEntry[]): IEntry[]
 
 export function cleanupTransfers(config: IConfiguration, entries: IEntry[]): IEntry[] {
     const prune = new Set()
-    for(const tx of entries.filter(e => e.metadata.ynab_transfer_id)) {
+    for(const tx of entries.filter(e => e.metadata?.ynab_transfer_id)) {
         if(!prune.has(tx.metadata.ynab_id)) {
             prune.add(tx.metadata.ynab_transfer_id)
         }
     }
-    return entries.filter(e => !(prune.has(e.metadata.ynab_id) || e.metadata.ynab_transfer_id === null));
+    return entries.filter(e => !e.metadata || !(prune.has(e.metadata.ynab_id) || e.metadata.ynab_transfer_id === null));
 }
 
 export function rtaToIncome(config: IConfiguration, entries: IEntry[]): IEntry[] {
